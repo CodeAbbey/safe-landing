d654ad664b5b0f4bdc508aa145a529594732bdda
Rodion Gorkovenko <rodiongork@github.com>
2014-10-11 09:23:14 +0400

    Regarding end-of-flight conditions

--- rocket.js
+++ rocket.js
@@ -12,11 +12,27 @@ var st = {
 };
 
 function report() {
-    term.print('t=' + st.time);
-    term.print('  ht=' + Math.round(st.height));
-    term.print('  spd=' + Math.round(st.speed));
-    term.println('  fuel=' + Math.round(st.fuel));
-    term.print('new burn rate: ');
+    if (st.fuel <= 0) {
+        term.printl('No more fuel!');
+    }
+    if (st.height <= 0) {
+        term.println('Touch down!');
+        term.println('Landing speed was: ' + Math.round(st.speed) + ' m/s');
+        if (st.speed < 5) {
+            term.println('Perfect landing, your contract is prolonged!');
+            term.println('Congratulations!');
+        } else if (st.speed < 50) {
+            term.println('You are still alive, but the craft is damaged!');
+            term.println('Waiting for the rescue team...');
+        } else {
+            term.println('CRASH! Your landing created a new crater');
+            term.println(Math.round(st.speed * 4) + ' meters deep!');
+        }
+    }
+    term.printf('%4d   %8d   %8d   %8d', st.time,
+            Math.round(st.height), Math.round(st.speed), Math.round(st.fuel));
+    term.println();
+    term.print('new burning rate: ');
 }
 
 function gets(str) {
@@ -32,14 +48,29 @@ function makeStep(rate) {
     var dm = rate * dt;
     for (var i = 0; i < steps; i++) {
         st.height -= st.speed * dt;
-        var dv = st.exhaust * dm / (st.mass + st.fuel);
+        if (st.height <= 0) {
+            st.time += st.tStep * (i + 1) / steps;
+            st.height = 0;
+            return;
+        }
+        var dv;
+        if (st.fuel <= dm) {
+            dm = st.fuel;
+        }
+        dv = st.exhaust * dm / (st.mass + st.fuel);
         st.fuel -= dm;
         st.speed += st.gravity * dt - dv;
     }
     st.time += st.tStep;
 }
 
-term.println('You are in a rocket approaching the Moon');
+term.println('You are in a rocket approaching the Moon!');
+term.println('Main computer failed (it was not built by DEC)!');
+term.println('You are to perform manual landing by controlling engines');
+term.println('specify fuel burning rate (kgs per second) for each 10 sec');
+term.println('and try to touch down with safe speed. Good luck!!!');
+term.println();
+term.println('Time   Height(m)   Speed(m/s)   Fuel(kg)');
 report();
 term.gets = gets;
 
